bcfc260f72ba24bbbbaef8dce90d55e1d3cf149e
glilley54 <glilley54@gmail.com>
2021-05-10 17:21:25 +0100

    film list completed

---
+++ src/components/FilmList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Film from './Film';
+
+const FilmList= ({films}) => {
+
+    const filmNodes = films.map(film => {
+        return (
+            <Film name ={film.name} key={film.id}>{film.url}</Film>
+        );
+    });
+
+    return(
+        <>
+        {filmNodes}    
+      </>    
+
+    )
+
+};
+
+
+export default FilmList;
